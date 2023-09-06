738643754b2850ac7b765289eb461dce60f2d43f
Washiil <65116802+Washiil@users.noreply.github.com>
2023-09-05 23:30:32 -0400

    slight change to table

--- src/lib/AdminView.svelte
+++ src/lib/AdminView.svelte
@@ -3,27 +3,33 @@
 	import type { TableSource } from '@skeletonlabs/skeleton';
 
 	const sourceData = [
-		{ position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-		{ position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-		{ position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-		{ position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-		{ position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
+		{ position: 1, email: 'example@gmail.com', grade: 11, expiration: '5 Days' },
+		{ position: 2, email: 'somethingcool@gmail.com', grade: 11, expiration: '5 Days' },
+		{ position: 3, email: 'othercoolthing@gmail.com', grade: 12, expiration: '5 Days' },
+		{ position: 4, email: 'somethjingelsecool@gmail.com', grade: 11, expiration: '5 Days' },
+		{ position: 5, email: 'polkadot@gmail.com', grade: 12, expiration: '5 Days' },
 	];
 
 	const tableSimple: TableSource = {
 		// A list of heading labels.
 		head: ['Email', 'Grade', 'Expires In'],
 		// The data visibly shown in your table body UI.
-		body: tableMapperValues(sourceData, ['name', 'symbol', 'weight']),
+		body: tableMapperValues(sourceData, ['email', 'grade', 'expiration']),
 		// Optional: The data returned when interactive is enabled and a row is clicked.
-		meta: tableMapperValues(sourceData, ['position', 'name', 'symbol', 'weight']),
+		meta: tableMapperValues(sourceData, ['position', 'email', 'grade', 'expiration']),
 		// Optional: A list of footer labels.
 		foot: ['Total', '', '<code class="code">5</code>']
 	};
+
+	function onSelected(meta: unknown): void {
+		console.log('on:selected', meta.detail);
+	}
 </script>
 
+<div class="text-left">
+	<Table source={tableSimple} interactive={true} on:selected={onSelected}/>
+</div>
 
-<Table source={tableSimple} />
 
 <style>
 
